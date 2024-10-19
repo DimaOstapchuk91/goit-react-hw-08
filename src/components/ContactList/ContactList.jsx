8c222a833afb693e1061c379d73969f18430a06b
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-10-19 16:35:45 +0300

    fix loader in contacts, dell css files

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,27 +2,35 @@ import Contact from '../Contact/Contact';
 import { useSelector } from 'react-redux';
 import Loader from '../Loader/Loader';
 import {
+  selectContacts,
   selectFilteredContacts,
   selectIsLoading,
 } from '../../redux/contacts/selectors';
 
 const ContactList = () => {
   const searchUsers = useSelector(selectFilteredContacts);
+  const contact = useSelector(selectContacts);
   const loader = useSelector(selectIsLoading);
 
   return (
     <div className='w-full'>
-      {loader && <Loader />}
-      <ul className='flex flex-col gap-6 '>
-        {searchUsers.map(user => (
-          <li
-            className='flex justify-between items-center p-4 w-full h-20 bg-light-blue rounded-xl shadow-custom-blue transition-all duration-300 hover:scale-105'
-            key={user.id}
-          >
-            <Contact user={user} />
-          </li>
-        ))}
-      </ul>
+      {contact.length > 0 ? (
+        <ul className='flex flex-col gap-6 '>
+          {loader && <Loader />}
+          {searchUsers.map(user => (
+            <li
+              className='flex justify-between items-center p-4 w-full h-20 bg-light-blue rounded-xl shadow-custom-blue transition-all duration-300 hover:scale-105'
+              key={user.id}
+            >
+              <Contact user={user} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className='font-bold text-center text-light-blue text-3xl'>
+          Please Add contact
+        </p>
+      )}
     </div>
   );
 };
